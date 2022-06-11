ed0a1a166dce366300910e4157f6dead1d22fde0
emidiotorre <emidio.torre@gmail.com>
2022-06-11 20:15:55 +0200

    6 binaries uploaded

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <Image src="public/1.JPG"></Image>
+            <Image src="/public/img/1.JPG" width="500" height="500"></Image>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
