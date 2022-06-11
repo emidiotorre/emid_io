e1ef427e05d2fb11ec3fe78d5b8f0577236c3dc1
emidiotorre <emidio.torre@gmail.com>
2022-06-11 22:41:54 +0200

    1 file modified, 1 added, 1 deleted

--- components/HomeSection.js
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-
-function Avatar() {
-  return <Image src="/me.png" alt="me" width="64" height="64" />;
-}
-
-export default Avatar;
---
+++ components/HomeSection.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const HomeSection: React.FC<{ title: string }> = ({ title }) => {
+  return <div>{title}</div>;
+};
+export default HomeSection;
